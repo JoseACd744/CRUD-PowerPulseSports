ab52ee2e4d4019de6dfe4986efeba7d26c801551
José Anthony Arenas Conde <142860250+JoseACd744@users.noreply.github.com>
2023-12-01 17:13:29 -0500

    Fix database connection string

--- src/database.js
+++ src/database.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 
 //ENV PROD
-//mongoose.connect('mongodb://database/powerpulse_db')
+mongoose.connect('mongodb://database/powerpulse_db')
 
 //ENV DEV
-mongoose.connect("mongodb://localhost:27017/powerpulsesports_db")
+//mongoose.connect("mongodb://localhost:27017/powerpulsesports_db")
 .then((db=>console.log("DB is connected to ", db.connection.host))).catch(err=>console.error(err));
 
 
